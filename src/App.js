fe58d6baa7187e2e1b8e0025b17f0f9cea55e10f
mrrocampbell <rocampbell@chegg.com>
2024-01-10 11:50:48 -0600

    Added solution code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+  useLocation
+} from "react-router-dom";
+
+
+function NoMatch() {
+  const location = useLocation();
+
+  return (
+    <h3>
+      No match for <code>{location.pathname}</code>
+    </h3>
+  );
+}
+
+function Home() {
+  return <p>Home</p>;
+}
+
+function About() {
+  return <p>About</p>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/about">About</Link>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
